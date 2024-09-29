518fe12f05fb68b8ea7e2907427547f55083115e
BryanArredon <164120862+BryanArredon@users.noreply.github.com>
2024-09-28 21:15:22 -0600

    Update keys.ts

--- src/keys.ts
+++ src/keys.ts
@@ -4,7 +4,7 @@ export default {
         host: 'database-1.cpi08i2y24t5.us-east-2.rds.amazonaws.com',
         user: 'root',
         password: 'brandon#12Gus',
-        database: 'citamedica',
+        database: 'cita',
         port: 3306
     }
 
